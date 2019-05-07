28a0071b9216742abc23f0424b859c5a5f247788
Nick Dichev <ndichev@barracuda.com>
2019-05-06 18:00:56 -0700

    Dont require the DEFAULT_MD environment variable.
    Instead, render a small default message.

--- lib/markdown_live_web/live/markdown_live.ex
+++ lib/markdown_live_web/live/markdown_live.ex
@@ -2,25 +2,41 @@ defmodule MarkdownLiveWeb.MarkdownLive do
   use Phoenix.LiveView
   alias MarkdownLiveWeb.MarkdownView
 
+  @default_template ~s"""
+  ## Markdown Live
+
+  Type Github Flavored Markdown and see it rendered in real time!
+
+  Note: If you're seeing this instead of some default Markdown you want to serve,
+  you forgot to set the `DEFAULT_MD` environment variable.
+
+  See the full code and documentation [here](https://github.com/nickdichev/markdown-live)
+  """
+
   def render(assigns) do
     MarkdownView.render("index.html", assigns)
   end
 
   def mount(_session, socket) do
-    default_template = 
-      File.read!(System.get_env("DEFAULT_MD"))
-    
-    default_md = 
+    default_template =
+      case System.get_env("DEFAULT_MD") do
+        nil ->
+          @default_template
+        path ->
+          File.read!(path)
+      end
+
+    default_md =
       Earmark.as_html!(default_template)
 
     {:ok, assign(socket, user_md: default_template, md_html: default_md)}
   end
 
   def handle_event("render", %{"user_md" => user_md}, socket) do
-    md_html = 
+    md_html =
       case Earmark.as_html(user_md) do
         {_, html_doc, _} ->
-          html_doc    
+          html_doc
     end
 
     {:noreply, assign(socket, user_md: user_md, md_html: md_html)}
